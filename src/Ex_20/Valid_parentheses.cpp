888ac47e4ddfbe15caf77bdbf18f2469b4d0c02e
liyixuan <liyixuan4030614@gmail.com>
2018-11-12 00:34:37 -0500

    exercise 20 and 21 (solved)

---
+++ src/Ex_20/Valid_parentheses.cpp
@@ -0,0 +1,64 @@
+//
+// Created by  liyixuan on 2018-11-11.
+//
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+using std::string;
+using std::stack;
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.length() == 0)
+            return true;
+
+        // stores each parenthese
+        stack<char> st;
+        int i = 0;
+        int length = s.length();
+        while (i < length) {
+            switch (s[i]) {
+                case '(':
+                    st.push(s[i]);
+                    break;
+                case '[':
+                    st.push(s[i]);
+                    break;
+                case '{':
+                    st.push(s[i]);
+                    break;
+                case ')':
+                    if (st.empty() || '(' != st.top())
+                        return false;
+                    st.pop();
+                    break;
+                case ']':
+                    if (st.empty() || '[' != st.top())
+                        return false;
+                    st.pop();
+                    break;
+                case '}':
+                    if (st.empty() || '{' != st.top())
+                        return false;
+                    st.pop();
+                    break;
+            }
+            i++;
+        }
+        if (st.empty())
+            return true;
+        return false;
+    }
+};
+
+int main() {
+    Solution s;
+    string str;
+    std::cin >> str;
+    std::cout << s.isValid(str) << std::endl;
+    return 0;
+}
---
+++ src/Ex_21/Merge_two_sorted_lists.cpp
@@ -0,0 +1,55 @@
+//
+// Created by  liyixuan on 2018-11-11.
+//
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr)
+            return l2;
+        if (l2 == nullptr)
+            return l1;
+        ListNode *n = new ListNode(-1);    // pointing to the result list's head
+        ListNode *temp = n; // iterate in the result list
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                ListNode *ln = new ListNode (l1->val);
+                temp->next = ln;
+                temp = ln;
+                l1 = l1->next;
+            } else {
+                ListNode *ln = new ListNode (l2->val);
+                temp->next = ln;
+                temp = ln;
+                l2 = l2->next;
+            }
+        }
+        while (l1) {
+            ListNode *ln = new ListNode (l1->val);
+            temp->next = ln;
+            temp = ln;
+            l1 = l1->next;
+        }
+        while (l2) {
+            ListNode *ln = new ListNode (l2->val);
+            temp->next = ln;
+            temp = ln;
+            l2 = l2->next;
+        }
+        return n->next;
+    }
+};
+
+int main() {
+
+    return 0;
+}
