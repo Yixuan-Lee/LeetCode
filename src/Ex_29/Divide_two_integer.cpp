1a6d2badd6565e5d346c68d48053afc93a1614ee
liyixuan <liyixuan4030614@gmail.com>
2018-11-20 00:54:42 -0500

    exercise 29 (solved)

---
+++ src/Ex_29/Divide_two_integer.cpp
@@ -0,0 +1,30 @@
+//
+// Created by  liyixuan on 2018-11-20.
+//
+
+#include <iostream>
+#include <cmath>
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (divisor == 0) {
+            return overflow;
+        }
+        long res = (long) dividend / divisor;
+        if (res < -pow(2, 31) || res > pow(2, 31) - 1) {
+            return overflow;
+        }
+        return static_cast<int>(res);
+    }
+
+private:
+    const int overflow = static_cast<int> (pow(2, 31) - 1);
+};
+
+int main() {
+    Solution s;
+    int diviend = -2147483648, divisor = -1;
+    std::cout << s.divide(diviend, divisor) << std::endl;
+    return 0;
+}
