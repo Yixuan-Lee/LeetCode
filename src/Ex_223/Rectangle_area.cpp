da16a0329359a2216b1ff3344a201e9fc418d899
liyixuan <liyixuan4030614@gmail.com>
2018-11-21 15:36:51 -0500

    exercise 223 (solved, challenge me through Gmail)

---
+++ src/Ex_223/Rectangle_area.cpp
@@ -0,0 +1,45 @@
+//
+// Created by  liyixuan on 2018-11-21.
+//
+// reference:
+//      http://www.cnblogs.com/grandyang/p/4563153.html
+
+#include <iostream>
+
+using std::min;
+using std::max;
+
+class Solution {
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        // sum area of two rectangles
+        int sum = (C - A) * (D - B) + (H - F) * (G - E);
+        // if there is no overlap
+        if (  (E >= C)
+           || (F >= D)
+           || (B >= H)
+           || (A >= G)) {
+            return sum;
+        }
+        // if there is an overlap
+        int overlap_length = min(C, G) - max(A, E); // x axis
+        int overlap_width  = min(D, H) - max(B, F); // y axis
+        int overlap = overlap_length * overlap_width;
+        sum = sum - overlap;
+        return sum;
+    }
+};
+
+int main() {
+    Solution s;
+    int A = -3;
+    int B = 0;
+    int C = 3;
+    int D = 4;
+    int E = 0;
+    int F = -1;
+    int G = 9;
+    int H = 2;
+    std::cout << s.computeArea(A, B, C, D, E, F, G, H) << std::endl;
+    return 0;
+}
