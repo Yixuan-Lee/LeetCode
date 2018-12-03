fb7dc829ceb2b5f7e7dab5cc3d763ca6c9ba5d72
liyixuan <liyixuan4030614@gmail.com>
2018-12-03 16:35:32 -0500

    exercise 58 (solved).

---
+++ src/Ex_58/Length_of_last_word.cpp
@@ -0,0 +1,42 @@
+//
+// Created by  liyixuan on 2018-12-03.
+//
+// references:
+//      https://leetcode.com/problems/length-of-last-word/discuss/21892/7-lines-4ms-C%2B%2B-Solution
+
+
+#include <iostream>
+#include <string>
+
+using std::string;
+
+class Solution {
+public:
+    int lengthOfLastWord (string s) {
+        auto length = static_cast<int>(s.length());
+        if (length == 0) {
+            return 0;
+        }
+        // find the index of last character
+        int tail = length - 1;
+        while (tail >= 0 && s[tail] == ' ') {
+            tail--;
+        }
+        // calculate the length of last word
+        int len = 0;
+        while (tail >= 0 && s[tail] != ' ') {
+            len++;
+            tail--;
+        }
+        return len;
+    }
+};
+
+
+int main () {
+    Solution s;
+    string input = "     ";
+    int length = s.lengthOfLastWord(input);
+    std::cout << length << std::endl;
+    return 0;
+}
