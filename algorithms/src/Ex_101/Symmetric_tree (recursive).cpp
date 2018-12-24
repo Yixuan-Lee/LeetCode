e89085f238435ba650543e55ff105ff16a85d08a
liyixuan <liyixuan4030614@gmail.com>
2018-12-24 07:08:45 -0500

    Symmetric tree; recursion, tree. solved [8ms 24.05%].

---
+++ algorithms/src/Ex_101/Symmetric_tree (recursive).cpp
@@ -0,0 +1,69 @@
+// references:
+//      https://leetcode.com/articles/symmetric-tree/
+
+
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        if (!root) {
+            return true;
+        }
+
+        return isMirror(root->left, root->right);
+    }
+
+private:
+    bool isMirror(TreeNode *p, TreeNode *q) {
+        if (!p && !q) {
+            return true;
+        }
+        if (!p || !q) {
+            return false;
+        }
+        // the reflection has the same head
+        // its left subtree corresponds to its reflection's right subtree
+        // its right subtree corresponds to tis reflection's left subtree
+        return (p->val == q->val)
+               && isMirror(p->left, q->right)
+               && isMirror(p->right, q->left);
+    }
+};
+
+
+int main() {
+    Solution s;
+    TreeNode a1(1), a2(2), a3(2), a4(3), a5(4), a6(4), a7(3);
+    a1.left = &a2;
+    a1.right = &a3;
+    a2.left = &a4;
+    a2.right = &a5;
+    a3.left = &a6;
+    a3.right = &a7;
+    std::cout << s.isSymmetric(&a1) << std::endl;
+
+    TreeNode b1(1), b2(2), b3(2), b4(3), b5(3);
+    b1.left = &b2;
+    b1.right = &b3;
+    b2.right = &b4;
+    b3.right = &b5;
+    std::cout << s.isSymmetric(&b1) << std::endl;
+
+    TreeNode c1(1), c2(2), c3(3), c4(3), c5(2);
+    c1.left = &c2;
+    c1.right = &c3;
+    c2.left = &c4;
+    c3.left = &c5;
+    std::cout << s.isSymmetric(&c1) << std::endl;
+
+    return 0;
+}
