93e8098b2a70714ec191ffde6a06d6cb3e922bbf
liyixuan <liyixuan4030614@gmail.com>
2019-01-05 00:22:19 -0500

    Number of 1 bits; bit manipulation, bit and. solved [4ms 76.74%].
    add reference links for printing uint32_t value.

--- algorithms/src/Ex_191/Number_of_1_bits_@_solution_N_&_N_minus_1.cpp
+++ algorithms/src/Ex_191/Number_of_1_bits_@_solution_N_&_N_minus_1.cpp
@@ -2,9 +2,12 @@
 //      http://www.cplusplus.com/reference/cstdint/
 //      https://en.wikipedia.org/wiki/Hamming_weight
 //      https://helloacm.com/c-coding-exercise-number-of-1-bits-revisited/
+//      https://stackoverflow.com/questions/43028865/how-to-print-hex-from-uint32-t
+//      https://stackoverflow.com/questions/12120426/how-do-i-print-uint32-t-and-uint16-t-variables-value
 
 
 #include <iostream>
+#include <inttypes.h>
 
 using std::uint32_t;
 
@@ -12,9 +15,9 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int r = n ? 1 : 0;
-        std::cout << n << std::endl;        // for debugging
+        printf("%" PRIu32 "\n", n);         // for debugging
         while (n &= (n - 1)) {
-            std::cout << n << std::endl;    // for debugging
+            printf("%08" PRIx32 "\n", n);   // for debugging, print in hex
 
             // n & (n - 1), remove the least-significant 1
             // remove 1 until the number n becomes 0
