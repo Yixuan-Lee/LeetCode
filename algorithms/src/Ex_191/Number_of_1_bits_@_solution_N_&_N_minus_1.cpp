4d509c39841dec8ecd0ee7d73d678d66c0d7d345
liyixuan <liyixuan4030614@gmail.com>
2019-01-04 23:59:42 -0500

    Number of 1 bits; bit manipulation, bit and. solved [4ms 76.74%].
    add debugging output.
    what does the least-significant 1 mean?

--- algorithms/src/Ex_191/Number_of_1_bits_@_solution_N_&_N_minus_1.cpp
+++ algorithms/src/Ex_191/Number_of_1_bits_@_solution_N_&_N_minus_1.cpp
@@ -12,7 +12,10 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int r = n ? 1 : 0;
+        std::cout << n << std::endl;        // for debugging
         while (n &= (n - 1)) {
+            std::cout << n << std::endl;    // for debugging
+
             // n & (n - 1), remove the least-significant 1
             // remove 1 until the number n becomes 0
             r++;
