52e670b5d7d4d1608e66941fb64bc0237d66eede
liyixuan <liyixuan4030614@gmail.com>
2019-01-04 21:50:28 -0500

    Valid phone number; shell script, grep, regular expression. solved [12ms 37.38%].
    add reference links.

--- shell/src/Ex_193/Valid_phone_numbers.sh
+++ shell/src/Ex_193/Valid_phone_numbers.sh
@@ -1,3 +1,7 @@
+# references:
+#       https://leetcode.com/problems/valid-phone-numbers/discuss/55478/Grep-e-solution-with-detailed-explanation-good-for-those-new-to-regex
+
+
 # Read from the file file.txt and output all valid phone numbers to stdout.
 # format matches 987-123-4567 or (123) 456-7890
 grep -e "^\([0-9]\{3\}-\|([0-9]\{3\})[ ]\{1\}\)[0-9]\{3\}-[0-9]\{4\}$" file.txt
