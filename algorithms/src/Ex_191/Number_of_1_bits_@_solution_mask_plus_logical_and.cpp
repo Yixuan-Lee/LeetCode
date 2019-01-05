e22b0b2721e2e51a944d02d552457a144bcb3c02
liyixuan <liyixuan4030614@gmail.com>
2019-01-05 00:30:24 -0500

    Number of 1 bits; bit manipulation, logical and, mask. solved [8ms 3.01%].

---
+++ algorithms/src/Ex_191/Number_of_1_bits_@_solution_mask_plus_logical_and.cpp
@@ -0,0 +1,33 @@
+// references:
+//      https://leetcode.com/articles/number-1-bits/
+
+
+#include <iostream>
+
+using std::uint32_t;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int bits = 0;
+        int mask = 1;   // 0000 0000 0000 0000 0000 0000 0000 0001
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                bits++;
+            }
+            mask <<= 1;
+        }
+        return bits;
+    }
+};
+
+
+int main() {
+    Solution s;
+    uint32_t n;
+
+    n = 00000000000000000000000000001011;
+    std::cout << s.hammingWeight(n) << std::endl;       // 3
+
+    return 0;
+}
