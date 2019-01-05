d1edadc9f6f8e9cfd9441d6d9183b7eb6bba28db
liyixuan <liyixuan4030614@gmail.com>
2019-01-05 10:35:29 -0500

    Reverse bits; bit manipulation. solved [8ms 6.24%].
    move pow(2, 31) outside of the for loop.

--- algorithms/src/Ex_190/Reverse_bits_@_solution_pow_plus_mask.cpp
+++ algorithms/src/Ex_190/Reverse_bits_@_solution_pow_plus_mask.cpp
@@ -12,11 +12,13 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
         int mask = 1;
+        auto feed = static_cast<uint32_t> (pow(2, 31));
         for (int i = 0; i < 32; i++) {
             if ((n & mask) != 0) {
-                ans += static_cast<uint32_t> (pow(2, 31 - i));
+                ans += feed;
             }
             mask <<= 1;
+            feed >>= 1;
         }
 
         return ans;
