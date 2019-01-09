0cb4137d0be11f36e5654bd08ee22f6270d77586
liyixuan <liyixuan4030614@gmail.com>
2019-01-08 22:11:56 -0500

    Bitwise AND of Numbers range; bit manipulation. not solved [40ms 50.66%].

---
+++ algorithms/src/Ex_201/Bitwise_and_of_numbers_range.cpp
@@ -0,0 +1,30 @@
+// references:
+//      https://leetcode.com/problems/bitwise-and-of-numbers-range/discuss/211810/C%2B%2B-O(32)-shift
+
+
+#include <iostream>
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int count = 0;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            count++;
+        }
+        return (m <<= count);
+    }
+};
+
+
+int main() {
+    Solution s;
+    std::cout << s.rangeBitwiseAnd(5, 7) << std::endl;              // 4
+    std::cout << s.rangeBitwiseAnd(0, 1) << std::endl;              // 0
+    std::cout << s.rangeBitwiseAnd(1, 2) << std::endl;              // 0
+    std::cout << s.rangeBitwiseAnd(1, 3) << std::endl;              // 0
+    std::cout << s.rangeBitwiseAnd(0, 2147483647) << std::endl;     // 0
+    std::cout << s.rangeBitwiseAnd(1, 2147483647) << std::endl;     // 0
+    return 0;
+}
