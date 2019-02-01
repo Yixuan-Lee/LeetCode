bbca678979a19f924e367019ea4b3579c9f48af5
liyixuan <liyixuan4030614@gmail.com>
2019-02-01 10:23:44 -0500

    Nim game; brainteaser. not solved [-ms -%  time limit exceeded].

--- algorithms/src/Ex_292/Nim_game_@_solution_recursion_with_memo.cpp
+++ algorithms/src/Ex_292/Nim_game_@_solution_recursion_with_memo.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 #include <vector>
 
+using std::vector;
+
 class Solution {
 public:
     bool canWinNim(int n) {
+        memo = vector<bool>(n + 1, false);
+        winlose = vector<bool>(n + 1, false);
+
         return canWinNim(n, true);
     }
 
 private:
+    vector<bool> memo;
+    vector<bool> winlose;
+
     bool canWinNim(int curr, bool turn) {
         if (curr == 1 || curr == 2 || curr == 3) {
             return turn;
         }
 
-        return canWinNim(curr - 1, !turn) || canWinNim(curr - 2, !turn)
-               || canWinNim(curr - 3, !turn);
+        if (memo[curr]) {
+            return winlose[curr];
+        }
+
+        bool factor3 = canWinNim(curr - 3, !turn);
+        memo[curr - 3] = true;
+        winlose[curr - 3] = factor3;
+
+        bool factor2 = canWinNim(curr - 2, !turn);
+        memo[curr - 2] = true;
+        winlose[curr - 2] = factor2;
+
+        bool factor1 = canWinNim(curr - 1, !turn);
+        memo[curr - 1] = true;
+        winlose[curr - 1] = factor1;
+
+        return factor1 || factor2 || factor3;
     }
 };
 
