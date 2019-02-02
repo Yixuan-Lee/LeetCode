45f6028f5eeae7af3af37260a16b5131f533633b
liyixuan <liyixuan4030614@gmail.com>
2019-02-02 10:25:59 -0500

    Power of four; bit manipulation. solved [Time: 4ms 97.12%, Memory: 745.5KB 92.86%].

---
+++ algorithms/src/Ex_342/Power_of_four_@_solution_bit_manipulation.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if (num <= 0) {
+            return false;
+        }
+
+        int remainder = 0;
+        while (num > 1 && remainder == 0) {
+            remainder = num & 3;
+            num = num >> 2;
+        }
+
+        return remainder == 0;
+    }
+};
+
+
+int main() {
+    Solution s;
+
+    std::cout << s.isPowerOfFour(16) << std::endl;  // 1
+
+    std::cout << s.isPowerOfFour(5) << std::endl;  // 0
+
+    std::cout << s.isPowerOfFour(2) << std::endl;  // 0
+
+    std::cout << s.isPowerOfFour(-2147483648) << std::endl;  // 0
+
+    std::cout << s.isPowerOfFour(1) << std::endl;  // 1
+
+    std::cout << s.isPowerOfFour(0) << std::endl;  // 0
+
+    return 0;
+}
