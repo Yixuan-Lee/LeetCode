e5ed4e7f4245e54128199d0138397882e388aa27
liyixuan <liyixuan4030614@gmail.com>
2020-09-26 12:33:51 -0400

    Related topics: linked list, two pointers.

---
+++ algorithms/src/Ex_141_linked_list_cycle/two_pointers.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+
+        slow = head
+        fast = head.next
+        while fast is not None and fast.next is not None:
+            if slow == fast:
+                return True
+            else:
+                slow = slow.next
+                fast = fast.next.next
+
+        return False
+
+
+def get_test_case_1() -> ListNode:
+    node_3 = ListNode(3)
+    node_2 = ListNode(2)
+    node_0 = ListNode(0)
+    node_n4 = ListNode(-4)
+
+    node_3.next = node_2
+    node_2.next = node_0
+    node_0.next = node_n4
+    node_n4.next = node_2
+    return node_3
+
+
+def get_test_case_3() -> ListNode:
+    node_1 = ListNode(1)
+    return node_1
+
+
+if __name__ == '__main__':
+    solu = Solution()
+    head1 = get_test_case_1()
+    print(solu.hasCycle(head1))
+
+    head3 = get_test_case_3()
+    print(solu.hasCycle(head3))
